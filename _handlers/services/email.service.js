d4b7b37867c486244faf7f3b4535afc30aa033c0
Andrey <andreylytovchenko@icloud.com>
2021-04-09 18:18:44 +0300

    fixed errors

--- _handlers/services/email.service.js
+++ _handlers/services/email.service.js
@@ -1,4 +1,5 @@
 import * as nodemailer from 'nodemailer';
+import { errorMiddleware } from '../middleware/error.middleware.js'
 
 export default class EmailService {
 
@@ -13,14 +14,22 @@ export default class EmailService {
         });
     }
 
-    send = (data) => {
+    send = (data, next) => {
+        let error = false;
+        let check;
         this._transporter.sendMail({
             from: "dre.mailerll@gmail.com",
             to: data.to,
             subject: data.subject,
             text: data.message,
-        }, (err) => {
-            if (err) throw new Error(err.message)
-        });
+        }, check = (err) => {
+            if (err) {
+                next(err)
+                return true
+            } 
+            return false
+        })
+        error = check()
+        return error
     }
 }
